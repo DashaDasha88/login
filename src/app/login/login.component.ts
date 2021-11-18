c8a6100c2ada88c3a388ad59a5c6de0acc5a19e3
Dasha <dasha.kotova.mail@gmail.com>
2021-11-17 20:32:59 -0500

    add authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -17,6 +18,9 @@ export class LoginComponent implements OnInit {
     const target = event.target
     const username = target.querySelector('#username').value
     const password = target.querySelector('#password').value
+
+    this.Auth.getUserDetails(username, password)
+
     console.log(username, password)
   }
 
